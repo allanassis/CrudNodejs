38b8cb9752f8695b0d1b78db2d000bec2a9a3bae
Allan Assis <allan-bdc@hotmail.com>
2018-03-09 23:27:11 -0300

    Ultimos ajustes!

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-let express = require('express');
+let express  = require('express');
 let mongoose = require('mongoose');
 
-let app = express();
-let db = mongoose.connection;
+let app  = express();
+let db   = mongoose.connection;
+let port = process.env.PORT || 8080;
 
 //mongoose.connect('mongodb://localhost:27017/');
-mongoose.connect("mongodb://adm:123456@ds231658.mlab.com:31658/nodeapplication")
-let port = process.env.PORT || 8080;
+mongoose.connect("mongodb://adm:123456@ds231658.mlab.com:31658/nodeapplication");
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 app.use(express.urlencoded(true));
 app.set('view engine', 'ejs');
+
 
 let Schema = mongoose.Schema;
 
@@ -20,11 +21,13 @@ let userSchema = new Schema({
     email: String,
     idade: Number
 })
+
 let User = mongoose.model('user', userSchema);
 
-
 app.get('/', (req, res) => {
+
     User.find(function(err, data){
+
         if(err) console.log(err);
 
         let usuario = data;
@@ -36,31 +39,44 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) =>{
+
     usuario = new User({
         nome : req.body.nome,
         email: req.body.email,
         idade: req.body.idade  
     })
+
     User.create(usuario, function(err, res){
+
         if(err) console.log(err);
+
         console.log("Dados Salvos!");
+
     });
+
     res.redirect('/');    
 })
 
 app.put('/:id', (req, res) => {
+
     let id_usuario = req.params.id;
+
     User.findByIdAndUpdate(id_usuario, req.body, function (err, resp) {
+
         if(err)console.log(err);
-        console.log(resp);        
+
         res.send(req.body);
     })    
 });
 
 app.delete('/:id', (req, res, next) => {
+
     let id_usuario = req.params.id;
+
      User.findByIdAndRemove(id_usuario, function(err, resp){
+
         if(err) console.error(err);
+
         res.end(res.nome);
     })
 })
@@ -69,9 +85,12 @@ app.delete('/:id', (req, res, next) => {
 db.on('error', console.error.bind(console,'Erro na conexão:'));
 
 db.once('open', function () {
+
     console.log("Conexão com o banco de dados estabelecida!");
 })
 
 app.listen(port, () => {
+
     console.log(`Servidor rodando na porta: ${port}`);
+    
 });
